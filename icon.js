dc04a7b136f5edbc06d88e6acad134c9f30ea44a
The Hangry Coder <thehangrycoder@The-Hangry-Coder.local>
2021-06-09 21:39:54 +0200

    added ascii lcars .. sort of

--- icon.js
+++ icon.js
@@ -1,5 +1,22 @@
+const { exit } = require('process');
 var fs = require('fs')
     , gm = require('gm').subClass({ imageMagick: true });
+
+
+const HEADER = `
+  ____________________________________________________             ____
+ /   ________________________________________________| REPLICATOR |____)
+|   |
+|___|
+`;
+console.log(HEADER);
+
+const BACKSLASH = '\\';
+const FOOTER = `
+|___|
+|   |___________________________________________________________   ____
+ ${BACKSLASH}______________________________________________________________| |____)
+`;
 
 
 // export path, make sure it exists
@@ -9,6 +26,7 @@ const EXPORT_PATH = "export/";
 const WORDS = [
     "HAIL"
 ];
+if(WORDS.length === 0) { console.log("|   | Enable to comply: no words parameters given!"); }
 
 // Add your colors, which will be used as backgroundcolors
 const COLORS = [
@@ -20,6 +38,7 @@ const COLORS = [
     "#0C3752",
     "#97CDF9"
 ];
+if(COLORS.length === 0) { console.log("|   | Enable to comply: no colors parameters given!"); }
 
 const ASSETS_FOLDER = "assets";
 const FONT = "Okuda.otf";
@@ -27,29 +46,28 @@ const FONT = "Okuda.otf";
 const WIDTH = 257;
 const HEIGHT = WIDTH;
 
-for(var x = 0 ;x < WORDS.length;x++) {
-   var _word    =  WORDS[x];
-   const _number = Math.floor((Math.random() * 8999) + 1000);
-   console.log(`Generating: ${_number} - ${_word}`);
-   var i;
-   for (i = 0; i < COLORS.length; i++) {
-       gm(WIDTH, HEIGHT, COLORS[i])
-       .font(`${ASSETS_FOLDER}${FONT}`,85)
-       .gravity("East")
-       .drawText(21,-2,_word.toUpperCase())
-       .font(`${ASSETS_FOLDER}${FONT}`,55)
-       .gravity("East")
-       .drawText(44,87,_number)
-       .stroke("#000000")
-       .strokeWidth(7)
-       .drawLine(0,172,WIDTH,172)
-           .write(`${EXPORT_PATH}${_word.toLowerCase()}_${i+1}.png`, function (err) {
-               if (err) {
-                   console.log(err);
-               }
-           });
-   } 
-   
-   
-}
+WORDS.forEach(_word => {
+    const _number = Math.floor((Math.random() * 8999) + 1000);
+    console.log(`|   | Generating: ${_number} - ${_word}`);
+    let colorIndex  = 0;
+    COLORS.forEach(color => {
+        gm(WIDTH, HEIGHT, color)
+        .font(`${ASSETS_FOLDER}${FONT}`,85)
+        .gravity("East")
+        .drawText(21,-2,_word.toUpperCase())
+        .font(`${ASSETS_FOLDER}${FONT}`,55)
+        .gravity("East")
+        .drawText(44,87,_number)
+        .stroke("#000000")
+        .strokeWidth(7)
+        .drawLine(0,172,WIDTH,172)
+            .write(`${EXPORT_PATH}${_word.toLowerCase()}_${colorIndex+1}.png`, function (err) {
+                if (err) {
+                    console.log(err);
+                }
+            });
+        colorIndex++;
+    });
+});
 
+console.log(FOOTER);
